5a0d2ee107092cf6d35655a0f3a7bdb1d1b0525a
Ajin <Ajin@DESKTOP-D41KSSQ>
2019-02-10 23:58:50 +0900

    nanumMember vo,service,dao 작성 nanum-mapper.xml 생성 및 등록

--- src/main/java/com/kh/duri/Nanummember/model/dao/NanumMemberDao.java
+++ src/main/java/com/kh/duri/Nanummember/model/dao/NanumMemberDao.java
@@ -1,5 +1,15 @@
 package com.kh.duri.Nanummember.model.dao;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.kh.duri.Nanummember.model.vo.Funding;
+import com.kh.duri.member.model.vo.Member;
+
 public interface NanumMemberDao {
+	
+	//진행중인 금액 크라우드펀딩 조회
+	List<Funding> selectMoneyCloud(SqlSessionTemplate sqlSession, Member m);
 
 }
--- src/main/java/com/kh/duri/Nanummember/model/service/NanumMemberServiceImpl.java
+++ src/main/java/com/kh/duri/Nanummember/model/service/NanumMemberServiceImpl.java
@@ -1,5 +1,26 @@
 package com.kh.duri.Nanummember.model.service;
 
-public class NanumMemberServiceImpl implements NanumMemberService {
+import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.kh.duri.Nanummember.model.dao.NanumMemberDao;
+import com.kh.duri.Nanummember.model.vo.Funding;
+import com.kh.duri.member.model.vo.Member;
+
+@Component
+public class NanumMemberServiceImpl implements NanumMemberService {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private NanumMemberDao nd;
+	
+	@Override
+	public List<Funding> selectMoneyCloud(Member m) {
+		List<Funding> fList = nd.selectMoneyCloud(sqlSession,m);
+		return fList;
+	}
 }
