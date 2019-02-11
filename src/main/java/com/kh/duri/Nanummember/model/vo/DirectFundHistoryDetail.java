44a0b7f84d41663ce991f7d776bac840b8490662
Ajin <Ajin@DESKTOP-D41KSSQ>
2019-02-12 00:38:24 +0900

    vo 작성 및 controller 수정

--- src/main/java/com/kh/duri/Nanummember/model/vo/DirectFundHistoryDetail.java
+++ src/main/java/com/kh/duri/Nanummember/model/vo/DirectFundHistoryDetail.java
@@ -9,5 +9,62 @@ public class DirectFundHistoryDetail implements java.io.Serializable{
 	private int dhdCount; //후원회차
 	private int dhdDhno; //정기후원내역
 	
+	public DirectFundHistoryDetail() {}
+
+	public DirectFundHistoryDetail(int dhdNo, Date dhdDate, int dhdValue, int dhdCount, int dhdDhno) {
+		super();
+		this.dhdNo = dhdNo;
+		this.dhdDate = dhdDate;
+		this.dhdValue = dhdValue;
+		this.dhdCount = dhdCount;
+		this.dhdDhno = dhdDhno;
+	}
+
+	@Override
+	public String toString() {
+		return "DirectFundHistoryDetail [dhdNo=" + dhdNo + ", dhdDate=" + dhdDate + ", dhdValue=" + dhdValue
+				+ ", dhdCount=" + dhdCount + ", dhdDhno=" + dhdDhno + "]";
+	}
+
+	public int getDhdNo() {
+		return dhdNo;
+	}
+
+	public void setDhdNo(int dhdNo) {
+		this.dhdNo = dhdNo;
+	}
+
+	public Date getDhdDate() {
+		return dhdDate;
+	}
+
+	public void setDhdDate(Date dhdDate) {
+		this.dhdDate = dhdDate;
+	}
+
+	public int getDhdValue() {
+		return dhdValue;
+	}
+
+	public void setDhdValue(int dhdValue) {
+		this.dhdValue = dhdValue;
+	}
+
+	public int getDhdCount() {
+		return dhdCount;
+	}
+
+	public void setDhdCount(int dhdCount) {
+		this.dhdCount = dhdCount;
+	}
+
+	public int getDhdDhno() {
+		return dhdDhno;
+	}
+
+	public void setDhdDhno(int dhdDhno) {
+		this.dhdDhno = dhdDhno;
+	}
+	
 	
 }
